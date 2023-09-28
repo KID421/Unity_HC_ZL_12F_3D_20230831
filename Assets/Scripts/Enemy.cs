1f2e528ebef1a43e19677ecb1ddefbbdcc026992
KID <50666207+KID421@users.noreply.github.com>
2023-09-28 21:47:10 +0800

    Update 完成

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
@@ -7,12 +8,19 @@ public class Enemy : MonoBehaviour
     private float speed = 3f;
     [SerializeField, Header("攻擊冷卻"), Range(0, 10)]
     private float attackCD = 4.5f;
+    [SerializeField, Header("攻擊區域")]
+    private GameObject goAttackArea;
+    [SerializeField, Header("啟動攻擊區域時間"), Range(0, 5)]
+    private float showAttackAreaTime = 1.5f;
+    [SerializeField, Header("啟動攻擊區域持續時間"), Range(0, 5)]
+    private float showAttackAreaDurationTime = 0.5f;
 
     private NavMeshAgent agent;
     private Transform target;
     private Animator ani;
     private string parWalk = "開關走路";
     private string parAttack = "觸發攻擊";
+    private bool canAttack = true; 
 
     private void Awake()
     {
@@ -22,13 +30,15 @@ public class Enemy : MonoBehaviour
         target = GameObject.Find("小黑").transform;
 
         ani = GetComponent<Animator>();
+
+        // StartCoroutine(Test());
     }
 
     private void Update()
     {
         agent.SetDestination(target.position);
 
-        print($"<color=#f69>距離：{agent.remainingDistance}</color>");
+        // print($"<color=#f69>距離：{agent.remainingDistance}</color>");
 
         if (agent.remainingDistance > agent.stoppingDistance)
         {
@@ -36,8 +46,36 @@ public class Enemy : MonoBehaviour
         }
         else if (agent.remainingDistance != 0)
         {
-            ani.SetTrigger(parAttack);
-            agent.isStopped = true;
+            if (canAttack) StartCoroutine(AttackEffect());
         }
+    }
+
+    private IEnumerator AttackEffect()
+    {
+        canAttack = false;
+        agent.isStopped = true;
+        ani.SetTrigger(parAttack);
+
+        Vector3 look = target.position;
+        look.y = transform.position.y;
+        transform.LookAt(look);
+
+        yield return new WaitForSeconds(showAttackAreaTime);
+        goAttackArea.SetActive(true);
+        yield return new WaitForSeconds(showAttackAreaDurationTime);
+        goAttackArea.SetActive(false);
+        ani.SetBool(parWalk, false);
+        yield return new WaitForSeconds(attackCD);
+        canAttack = true;
+        agent.isStopped = false;
+    }
+
+    private IEnumerator Test()
+    {
+        print("嗨，我是第一行");
+        yield return new WaitForSeconds(1);
+        print("第二行~");
+        yield return new WaitForSeconds(2);
+        print("第三行~");
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using StarterAssets;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("老狼")]
+    public Enemy enemy;
+    [Header("老狼動畫")]
+    public Animator aniEnemy;
+
+    [Header("小黑動畫")]
+    public Animator aniPlayer;
+    [Header("小黑角色控制器")]
+    public CharacterController characterPlayer;
+    [Header("小黑第三人稱控制系統")]
+    public ThirdPersonController controllerPlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print($"<color=#f69>碰到的物件：{other}</color>");
+
+        if (other.name.Contains("過關區域")) Pass();
+        if (other.name.Contains("老狼攻擊區域")) Lose();
+    }
+
+    private void Pass()
+    {
+        aniEnemy.SetBool("開關走路", false);
+        enemy.enabled = false;
+    }
+
+    private void Lose()
+    {
+        aniPlayer.enabled = false;
+        characterPlayer.enabled = false;
+        controllerPlayer.enabled = false;
+    }
+}
